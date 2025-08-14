0d564d63425c56b5dbb3f1613ee2bec97ae9c85f
Ahmad-ctrl-s <ahmaddakouri6@gmail.com>
2025-08-14 18:21:20 +0300

    Update spa-404.cjs

--- scripts/spa-404.cjs
+++ scripts/spa-404.cjs
@@ -2,7 +2,8 @@ import { copyFileSync } from 'fs';
 
 try {
   copyFileSync('dist/index.html', 'dist/404.html');
-  console.log('✔ Created dist/404.html');
+  console.log('✔ Created dist/404.html for SPA fallback');
 } catch (err) {
   console.error('✖ Could not create 404.html', err);
 }
+
