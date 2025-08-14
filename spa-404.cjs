d13c3b3ea103f102712220506a3eb5e09514b0cc
Ahmad-ctrl-s <ahmaddakouri6@gmail.com>
2025-08-14 18:07:40 +0300

    Delete spa-404.cjs file after adding it to scripts folder

--- spa-404.cjs
+++
@@ -1,8 +0,0 @@
-const { copyFileSync } = require('fs');
-
-try {
-  copyFileSync('dist/index.html', 'dist/404.html');
-  console.log('✔ Created dist/404.html');
-} catch (err) {
-  console.error('✖ Could not create 404.html', err);
-}
