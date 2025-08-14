8b19900f4ab73929e310419475d28cd049a66836
Ahmad-ctrl-s <ahmaddakouri6@gmail.com>
2025-08-14 17:49:44 +0300

    Update main.jsx importing BrowserRouter and BaseName

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+
+const basename = import.meta.env.PROD ? '/Articula' : '/';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
